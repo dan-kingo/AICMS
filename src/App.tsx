a1daf7995d47ef33d833a89d57d99501ba7736c9
Dan Kingo <danieldejen23@gmail.com>
2025-03-02 12:10:41 +0300

    install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1>AICMS</h1>
+      <h1 className="text-3xl font-bold">AICMS</h1>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // adjust the paths according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
