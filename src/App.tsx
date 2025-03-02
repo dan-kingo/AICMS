c9310dfd363d75fbde4e72e9bcddc0e8abad22d2
Dan Kingo <danieldejen23@gmail.com>
2025-03-02 19:30:20 +0300

    create a basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Home } from "./pages";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
