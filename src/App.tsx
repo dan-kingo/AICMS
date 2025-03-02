0fb6462a408ef0aeeccea50d8d62ed29e82db046
Dan Kingo <danieldejen23@gmail.com>
2025-03-02 12:20:56 +0300

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold">AICMS</h1>
+      <Button className="bg-amber-400">Submit</Button>
     </div>
   );
 };
