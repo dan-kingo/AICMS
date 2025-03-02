1bdcb8cad92477db754d4a2587ebce1cb7a3c3c1
Dan Kingo <danieldejen23@gmail.com>
2025-03-02 15:24:04 +0300

    add dark mode component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-import { Flashlight, Lightbulb, Zap, ZapIcon, ZapOff } from "lucide-react";
+import { ZapIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import DarkModeToggle from "./DarkModeToggle";
 
 const Navbar = () => {
   return (
-    <div className="bg-white py-4 p-20 flex justify-between">
+    <div className="dark:bg-dark bg-white py-4 p-20 flex justify-between">
       <div className="flex items-center ">
         <ZapIcon size="32px" className="text-primary" />
         <p className="ps-2 font-semibold text-xl">EEU Compliant System</p>
@@ -13,9 +14,9 @@ const Navbar = () => {
         <a href="">About</a>
         <a href="">Help/FAQ</a>
         <a href="">Contact Us</a>
-        <a href="">Dark Mode</a>
+        <DarkModeToggle />
         <Button variant="outline">Login</Button>
-        <Button>Register</Button>
+        <Button className="dark:text-white">Register</Button>
       </div>
     </div>
   );
