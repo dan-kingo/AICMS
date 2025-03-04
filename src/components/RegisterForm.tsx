052645433ed182a38dd055ae5695aaa89e297c71
Dan Kingo <danieldejen23@gmail.com>
2025-03-04 09:44:02 +0300

    create the UI of the page

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,7 +1,91 @@
-import React from "react";
+import { contactFormSchema } from "@/utils/contactFormSchema";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { useState } from "react";
+import registerData from "@/assets/constants/registerData";
 
 const RegisterForm = () => {
-  return <div>Register Form</div>;
+  const [isLoading, setLoading] = useState(false);
+  const form = useForm();
+  return (
+    <div className="dark:bg-dark p-4 bg-white  w-96 flex flex-col rounded-lg shadow-lg">
+      <h1 className="md:text-xl font-semibold  pb-6 text-lg font-palanquin flex justify-center w-full">
+        Create Your Account
+      </h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(() => console.log("Submitted!"))}
+          className="w-full  space-y-6 "
+        >
+          {registerData.map((formInput, index) => (
+            <FormField
+              key={index}
+              control={form.control}
+              name={formInput.name as keyof z.infer<typeof contactFormSchema>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{formInput.label}</FormLabel>
+                  <FormControl>
+                    <Input
+                      type={formInput.type}
+                      className="placeholder:text-[13px] md:placeholder:text-[14px] rounded-full"
+                      placeholder={formInput.placeholder}
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          {/* <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Gender</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger className="w-full rounded-full">
+                      <SelectValue placeholder="Enter your gender" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="Male">Male</SelectItem>
+                    <SelectItem value="Female">Female </SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          /> */}
+
+          <Button
+            className="dark:text-white rounded-full w-full"
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading ? "Registering..." : "Register"}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default RegisterForm;
