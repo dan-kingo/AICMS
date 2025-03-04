707d884caf521ca8d62db27c09d102837892fec5
Dan Kingo <danieldejen23@gmail.com>
2025-03-04 18:51:09 +0300

    finish about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,43 +1,21 @@
-import Typed from "typed.js";
-
+import Footer from "@/components/Footer";
 import aboutImg from "../assets/undraw_electricity_iu6d.svg";
 import FeatureCard from "@/components/FeatureCard";
-import { useRef, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { NavLink } from "react-router-dom";
 const About = () => {
-  const typedRef = useRef<HTMLSpanElement>(null);
-
-  useEffect(() => {
-    const typed = new Typed(typedRef.current, {
-      strings: [
-        "Complaint Management",
-        "Customer Service",
-        "Customer Experience",
-      ],
-      typeSpeed: 150,
-      backSpeed: 130,
-      backDelay: 1000,
-      startDelay: 500,
-      loop: true,
-      showCursor: true,
-    });
-
-    return () => {
-      typed.destroy();
-    };
-  }, []);
-
   return (
-    <div className="md:mx-20 mx-4 my-12 mt-24 min-h-about">
+    <div className="md:mx-20 mx-4 py-12 mt-24 min-h-about">
       {/* Hero section  */}
       <div className="flex w-full gap-12 pt-6 py-24">
         <img src={aboutImg} alt="about image" className="hidden md:block" />
         <div className="flex flex-4/5  w-full flex-col justify-center">
           <h1 className="pb-4  md:text-7xl text-4xl font-semibold font-palanquin text-center dark:text-gray-300 text-gray-900">
             Bridging Gaps in{" "}
-            <span
-              ref={typedRef}
-              className="bg-gradient-to-r  from-primary to-blue-700 bg-clip-text text-transparent"
-            ></span>{" "}
+            <span className="bg-gradient-to-r  from-primary to-blue-700 bg-clip-text text-transparent">
+              {" "}
+              Customer Service
+            </span>{" "}
             with AI.
           </h1>
         </div>
@@ -46,13 +24,29 @@ const About = () => {
       {/* Feature section */}
 
       <div>
-        <h1 className="md:text-3xl text-2xl font-palanquin font-semibold  text-center pb-6">
+        <h1 className="md:text-4xl text-3xl font-palanquin font-semibold  text-center pb-6">
           Our Features
         </h1>
         <div className="flex justify-around gap-6 py-8 md:flex-nowrap flex-wrap">
           <FeatureCard />
         </div>
       </div>
+
+      {/* get in touch section  */}
+
+      <div className="mt-24 mb-12  flex flex-col justify-center items-center gap-4 ">
+        <h1 className="md:text-4xl text-3xl font-palanquin font-semibold">
+          Get In Touch
+        </h1>
+        <p className="text-center dark:text-gray-400">
+          Have questions? Contact us for more information.
+        </p>
+        <NavLink to="/contact-us">
+          {" "}
+          <Button className="dark:text-white">Contact Us</Button>
+        </NavLink>
+      </div>
+      <Footer />
     </div>
   );
 };
