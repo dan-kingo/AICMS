0eb0e5274a47c78a8851e1d632ed160760671e29
Dan Kingo <danieldejen23@gmail.com>
2025-03-06 15:12:43 +0300

    implement basic FAQ page

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,7 +1,22 @@
+import FAQImage from "../assets/FAQs-amico.svg";
+
 const Help = () => {
   return (
     <div className="md:mx-20 mx-4 my-12 mt-24">
-      <h1 className="text-3xl">Help/FAQ</h1>
+      <div className="flex  gap-12 pt-6 py-24">
+        <div className="w-full">
+          <h1 className="md:pt-12 md:pb-6 font-bold font-palanquin md:mx-6 md:text-6xl text-4xl dark:text-gray-200 text-gray-900">
+            Frequenty Asked Questions
+          </h1>
+        </div>
+        <div className="w-full items-center  justify-center hidden md:flex ">
+          <img
+            src={FAQImage}
+            alt="FAQ Image"
+            className="w-[450px] hidden md:block dark:opacity-80"
+          />
+        </div>
+      </div>
     </div>
   );
 };
