4043d4df6129a6c8b9280faa739a87e21133ea4c
Dan Kingo <danieldejen23@gmail.com>
2025-03-07 18:33:51 +0300

    create image Upload component

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,7 +1,10 @@
+import ProfilePictureUpload from "@/components/ProfilePictureUpload";
+
 const UserProfile = () => {
   return (
     <div>
       <h1 className="text-3xl">User Profile</h1>
+      <ProfilePictureUpload />
     </div>
   );
 };
