55e0446b658863a048c145c02c69e2e008c35a1e
Dan Kingo <danieldejen23@gmail.com>
2025-03-08 15:54:35 +0300

    fix footer issue in dashboards

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
+import { useLocation } from "react-router-dom";
+
 const Footer = () => {
+  const location = useLocation();
+  let isDashboardPath = location.pathname.startsWith("/dashboard");
+
   return (
     <div className="pt-12 ">
-      <p className="text-center dark:text-gray-600 text-gray-400">
+      <p
+        className={`text-center dark:text-gray-600 text-gray-400 ${
+          isDashboardPath ? "md:ml-64" : ""
+        }`}
+      >
         &copy; {new Date().getFullYear()} Ethiopian Electric Utility. All Rights
         Reserved.
       </p>
