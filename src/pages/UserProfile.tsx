ba8092c2b939bc1ddfb86f896765fcaaf3ecd074
Dan Kingo <danieldejen23@gmail.com>
2025-03-19 09:45:52 +0300

    sync user name with database

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,7 +1,24 @@
 import EditProfileForm from "@/components/EditProfileForm";
 import ProfilePictureUpload from "@/components/ProfilePictureUpload";
+import useUser from "@/hooks/useUser";
+import { useState, useEffect } from "react";
+
+interface User {
+  firstName: string;
+  [key: string]: any;
+}
 
 const UserProfile = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await useUser();
+      setUser(userData);
+    };
+
+    fetchUser();
+  }, []);
   return (
     <div className="md:ms-12">
       {/* profile picture */}
@@ -9,9 +26,9 @@ const UserProfile = () => {
         <ProfilePictureUpload />
         <div>
           <h3 className="md:text-2xl text-xl font-bold font-palanquin">
-            John Smith
+            {user?.firstName + " " + user?.lastName}
           </h3>
-          <p className="text-primary">@johnas</p>
+          <p className="text-primary">{user?.userName}</p>
         </div>
       </div>
 
