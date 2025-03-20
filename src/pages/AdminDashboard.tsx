36972f8dc4fd7f843e0e2b053383f6a2a06ebed6
Dan Kingo <danieldejen23@gmail.com>
2025-03-20 14:24:49 +0300

    use shadcn chart component

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,7 +1,20 @@
+import CustomBar from "@/components/CustomBar";
+import CustomSidebar from "@/components/CustomSidebar";
+import DashboardNavbar from "@/components/DashboardNavbar";
+
 const AdminDashboard = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Admin Dashboard</h1>
+    <div className="min-h-screen flex flex-col ">
+      <DashboardNavbar />
+
+      <div className="flex flex-1">
+        <aside className="w-64 hidden md:block">
+          <CustomSidebar />
+        </aside>
+        <main className="flex-1   p-6 sm:p-4 sidebar-height overflow-auto ">
+          <CustomBar />
+        </main>
+      </div>
     </div>
   );
 };
